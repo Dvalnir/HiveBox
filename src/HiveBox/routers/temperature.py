235a003d1df6b9a2c1e8a1856f7dc319d104065c
Dvalnir <47464326+Dvalnir@users.noreply.github.com>
2025-04-26 03:45:21 +0200

    feat: Extracting function get_box_temp_if_eligible from get_average_temp

--- src/HiveBox/routers/temperature.py
+++ src/HiveBox/routers/temperature.py
@@ -20,24 +20,34 @@ async def get_average_temp() -> float:
         "5ade1acf223bd80019a1011c",
     ]
     measurement_last_acceptable_time = datetime.now(timezone.utc) - timedelta(hours=1)
-    # 2025-04-21T12:55:17.511Z where Z is Zulu time => UTC+0
-    opensense_time_format = "%Y-%m-%dT%H:%M:%S.%f%z"
     temperature_list = []
     for sensebox_ID in sensebox_ID_list:
         api_url = f"https://api.opensensemap.org/boxes/{sensebox_ID}"
-        result = requests.get(url=api_url, timeout=0.3)
-        if result.status_code == 200:
-            for sensor in result.json()["sensors"]:
-                if sensor["title"] == "Temperatur":
-                    last_measurement = sensor["lastMeasurement"]
-                    measurement_time = datetime.strptime(
-                        last_measurement["createdAt"], opensense_time_format
-                    )
-                    if measurement_time >= measurement_last_acceptable_time:
-                        temperature_list.append(float(last_measurement["value"]))
-        else:
-            print(f"{api_url} resulted in status code {result.status_code}")
+        temp = get_box_temp_if_eligible(measurement_last_acceptable_time, api_url)
+        if temp is not None:
+            temperature_list.append(temp)
     if len(temperature_list) == 0:
         return -273.15
     else:
         return fmean(temperature_list)
+
+
+def get_box_temp_if_eligible(measurement_last_acceptable_time, api_url) -> float | None:
+    """Retrieve a senseBox temperature."""
+    box_temperature = None
+    # 2025-04-21T12:55:17.511Z where Z is Zulu time => UTC+0
+    opensense_time_format = "%Y-%m-%dT%H:%M:%S.%f%z"
+    request_timeout = 0.3
+    result = requests.get(url=api_url, timeout=request_timeout)
+    if result.status_code == 200:
+        for sensor in result.json()["sensors"]:
+            if sensor["title"] == "Temperatur":
+                last_measurement = sensor["lastMeasurement"]
+                measurement_time = datetime.strptime(
+                    last_measurement["createdAt"], opensense_time_format
+                )
+                if measurement_time >= measurement_last_acceptable_time:
+                    box_temperature = float(last_measurement["value"])
+    else:
+        print(f"{api_url} resulted in status code {result.status_code}")
+    return box_temperature
