351cbdf33806ccd3ce9b3737349902d4e05226a6
Dvalnir <47464326+Dvalnir@users.noreply.github.com>
2026-05-14 23:02:19 +0000

    feat: Adding the /metrics endpoint

--- src/hive_box/main.py
+++ src/hive_box/main.py
@@ -4,12 +4,16 @@ Main module which also define the root endpoint.
 
 from fastapi import FastAPI
 from .routers import version, temperature
+from prometheus_client import make_asgi_app
 
 app = FastAPI()
 
 app.include_router(version.router)
 app.include_router(temperature.router)
 
+# Add prometheus asgi middleware to route /metrics requests
+metrics_app = make_asgi_app()
+app.mount("/metrics", metrics_app)
 
 @app.get("/")
 async def root():
